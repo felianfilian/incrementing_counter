6fb1176a239f9ea019d4a2be07173b734a17e051
Dennis Schmalstieg <darianbellentor@outlook.de>
2022-11-11 00:43:20 +0100

    get attribute number

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
+const counters = document.querySelectorAll(".counter");
 
+counters.forEach((counter) => {
+  counter.innerHTML = "0";
+  const updateCounter = () => {
+    const target = +counter.getAttribute("data-target");
+    const c = +counter.innerText;
+  };
+});
