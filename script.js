0a8c68a1ebc9882a81b3915677116cfc7dbccd2c
Dennis Schmalstieg <darianbellentor@outlook.de>
2022-11-12 09:41:23 +0100

    show counter

--- script.js
+++ script.js
@@ -5,5 +5,13 @@ counters.forEach((counter) => {
   const updateCounter = () => {
     const target = +counter.getAttribute("data-target");
     const c = +counter.innerText;
+
+    const increment = target / 200;
+
+    if (c < target) {
+      counter.innerText = `${Math.ceil(c + increment)}`;
+    }
   };
+
+  updateCounter();
 });
